d827a4873ff7cbd0d205a735cc2bdac62810503a
Raafat <raafatm2001@gmail.com>
2025-04-14 23:16:52 +0200

    Add the add Note Model and Adpter that

---
+++ lib/features/add_note/data/model.dart
@@ -0,0 +1,21 @@
+import 'package:hive/hive.dart';
+
+part 'model.g.dart';
+@HiveType(typeId: 0)
+class AddNoteModel{
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String desc;
+  @HiveField(2)
+  final DateTime  createdAt;
+  @HiveField(3)
+  final int color;
+  AddNoteModel( {
+    required this.title,
+    required this.desc,
+    required this.createdAt,
+    required this.color,
+  });
+
+}
